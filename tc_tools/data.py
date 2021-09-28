a0dcffd6da5e3f4105330bda4f6475c500df8ba0
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2021-09-28 22:05:15 +0200

    Added data.py containing the Variable class

---
+++ tc_tools/data.py
@@ -0,0 +1,70 @@
+"""
+Module containing everything to handle data processing.
+"""
+
+
+class Source:
+    """
+    Class handling the csv sources.
+    """
+    pass
+
+
+_global_source = Source()
+
+
+class Variable:
+    """
+    A class corresponding to a constant or to an attribute of the data source.
+
+    Parameter
+    ----------
+
+    val: int or str,
+        either the corresponding constant or the name of the corresponding attribute
+
+    Properties
+    ----------
+
+    field : str,
+        the name of the corresponding attribute or None if corresponding to a constant
+
+    val : int,
+        the corresponding constant, or None if the object corresponds to an attribute
+
+    """
+    def __init__(self, val):
+        if isinstance(val, int):
+            self._val = val
+            self._field = None
+        elif isinstance(val, str):
+            self._val = None
+            self._field = val
+        else:
+            raise ValueError("For initialization of Variable, only int and str are allowed")
+
+    @property
+    def val(self):
+        return self._val
+
+    @property
+    def field(self):
+        return self._field
+
+    @val.setter
+    def val(self, val):
+        if isinstance(val, int):
+            self._val = val
+            self._field = None
+
+    @field.setter
+    def field(self, field):
+        if isinstance(field, str):
+            self._val = None
+            self._field = field
+
+    def __call__(self):
+        if self.val:
+            return self.val
+        if self.field:
+            return _global_source(self.field)
