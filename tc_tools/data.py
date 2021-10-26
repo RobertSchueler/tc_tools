3ce9928a4b0bac141752d090090fcd5dbde3e22c
Valentin Dannenberg <71141828+rubbinduck@users.noreply.github.com>
2021-10-26 16:18:48 +0200

    reworked Variable into a more fitting dataclass

--- tc_tools/data.py
+++ tc_tools/data.py
@@ -2,6 +2,10 @@
 Module containing everything to handle data processing.
 """
 
+# read up on dataclasses, they are cool!
+from dataclasses import dataclass
+from typing import Optional
+
 
 class Source:
     """
@@ -13,6 +17,7 @@ class Source:
 _global_source = Source()
 
 
+@dataclass
 class Variable:
     """
     A class corresponding to a constant or to an attribute of the data source.
@@ -24,44 +29,19 @@ class Variable:
         either the corresponding constant or the name of the corresponding attribute
 
     """
-    def __init__(self, val):
-        if isinstance(val, int):
-            self._val = val
-            self._field = None
-        elif isinstance(val, str):
-            self._val = None
-            self._field = val
-        else:
-            raise ValueError("For initialization of Variable, only int and str are allowed")
-
-    @property
-    def val(self):
-        """
-        the corresponding constant, or None if the object corresponds to an attribute
-        """
-        return self._val
-
-    @property
-    def field(self):
-        """
-        the name of the corresponding attribute or None if corresponding to a constant
-        """
-        return self._field
-
-    @val.setter
-    def val(self, val):
-        if isinstance(val, int):
-            self._val = val
-            self._field = None
-
-    @field.setter
-    def field(self, field):
-        if isinstance(field, str):
-            self._val = None
-            self._field = field
-
+    value: Optional[int] = None
+    field_name: Optional[str] = None
+        
+    def __post_init__(self):
+        # check if at least one attribute is initialized
+        if not field_name or value is None: # check for None, bc 0 is poss.
+            raise ValueError(
+                "At least one of `value` or `field_name` must be set"
+            )
+    
     def __call__(self):
-        if self.val:
-            return self.val
-        if self.field:
-            return _global_source(self.field)
+        return (
+            self.value 
+            if self.value is not None # check for None, bc 0 is poss.
+            else _global_source(self.field_name)
+        )
